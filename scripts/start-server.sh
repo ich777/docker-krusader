4f1db615053fd15d163c1672124547620c312159
ich777 <ich777@noreply.example.org>
2021-04-13 14:33:19 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,12 +3,19 @@ export DISPLAY=:99
 export XDG_RUNTIME_DIR=/tmp/xdg
 export LANGUAGE="$LOCALE_USR"
 export LANG="$LOCALE_USR"
+export XAUTHORITY=${DATA_DIR}/.Xauthority
 
 echo "---Checking for old logfiles---"
 find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
 find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
 echo "---Checking for old display lock files---"
-find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
+rm -rf /tmp/.X99*
+rm -rf /tmp/.X11*
+rm -rf ${DATA_DIR}/.vnc/*.log ${DATA_DIR}/.vnc/*.pid
+chmod -R ${DATA_PERM} ${DATA_DIR}
+if [ -f ${DATA_DIR}/.vnc/passwd ]; then
+	chmod 600 ${DATA_DIR}/.vnc/passwd
+fi
 screen -wipe 2&>/dev/null
 
 echo "---Resolution check---"
@@ -28,13 +35,8 @@ if [ "${CUSTOM_RES_H}" -le 767 ]; then
     CUSTOM_RES_H=768
 fi
 
-chmod -R ${DATA_PERM} ${DATA_DIR}
-
-echo "---Starting Xvfb server---"
-screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
-sleep 2
-echo "---Starting x11vnc server---"
-screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
+echo "---Starting TurboVNC server---"
+vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
 sleep 2
 echo "---Starting Fluxbox---"
 screen -d -m env HOME=/etc /usr/bin/fluxbox
