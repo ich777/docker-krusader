43333431d22795ea1fda393413e1649f3b0a3b5b
Christoph Hummer <christophhummer@gmail.com>
2024-12-19 08:26:53 +0100

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -40,7 +40,7 @@ if [ "${CUSTOM_RES_H}" -le 767 ]; then
 fi
 
 echo "---Starting TurboVNC server---"
-vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
+vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup -noserverkeymap ${TURBOVNC_PARAMS} 2>/dev/null
 sleep 2
 echo "---Starting Fluxbox---"
 screen -d -m env HOME=/etc /usr/bin/fluxbox
